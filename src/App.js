1134d0a8d108880ddebb9ba4696c986e4f6601ca
tayyab2468 <ranatayyabcriclover147@gmail.com>
2024-10-31 23:39:14 +0500

    Add style.css file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "./style.css";
 
 function App() {
+  const [weight, setWeight] = useState("");
+  const [height, setheight] = useState("");
+
+  const [bmi, setBmi] = useState("");
+  const [category, setCategory] = useState("");
+  const [color, setcolor] = useState("");
+
+  const getBmi = () => {
+    if (height && weight) {
+      const heightInMeters = height / 100;
+      const bmiValue = parseFloat(weight) / heightInMeters ** 2;
+
+      setBmi(bmiValue.toFixed(2));
+
+     
+      if (bmiValue < 18.5) {
+        setCategory("Underweight"); 
+      } else if (bmiValue < 24.9) {
+        setCategory("Normal Weight");
+        setcolor("green");
+      } else if (bmiValue < 29.9) {
+        setCategory("Overweight");
+        setcolor("orange");
+      } else {
+        setCategory("Extremly Fat Dangerous for Health");
+        setcolor(" red");
+      }
+    } else {
+      setBmi("Invalid input");
+      setCategory(""); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>BMI Calculator</h1>
+      <label>
+        Weight <span>(Kg)</span>
+      </label>
+      <input
+        type="text"
+        onChange={(e) => setWeight(e.target.value)}
+        placeholder="Enter Weight"
+      />
+
+      <label>Height (cm)</label>
+      <input
+        type="text"
+        onChange={(e) => setheight(e.target.value)}
+        placeholder="Enter height in feet"
+      />
+
+      <button onClick={getBmi}>Submit</button>
+      <p>The BMI is: {bmi}</p>
+      <h2 style={{ color: color }}> {category}</h2>
     </div>
   );
 }
+}
 
 export default App;
+
